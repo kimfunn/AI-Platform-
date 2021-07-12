92f108a9adcc393c6258c6fc81acde6a1edce598
jaemin <kimfunn@naver.com>
2021-07-12 14:56:29 +0900

    20_cafe webcam add

---
+++ springboot(2106)/semi_pj/src/main/java/my/jes/web/service/OrderService.java
@@ -0,0 +1,18 @@
+package my.jes.web.service;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import my.jes.web.dao.OrderDAO;
+import my.jes.web.vo.OrderVO;
+
+@Service
+public class OrderService {
+	@Autowired
+	OrderDAO orderDAO;
+	
+	public long ordersInsert(ArrayList<OrderVO> list) throws Exception{
+		return orderDAO.ordersInsert(list);
+		}
+	}
