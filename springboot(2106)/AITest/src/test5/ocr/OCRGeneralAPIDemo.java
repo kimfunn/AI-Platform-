fecef5e16777fda951bc6d215b9e46ec38198ed7
jaemin <kimfunn@naver.com>
2021-07-20 11:10:47 +0900

    ocr ofr semi_readme add

---
+++ springboot(2106)/AITest/src/test5/ocr/OCRGeneralAPIDemo.java
@@ -0,0 +1,109 @@
+package test5.ocr;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.UUID;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class OCRGeneralAPIDemo {
+
+	public static void main(String[] args) {
+		String apiURL = "https://67bbf3df9be646328cb58f92c87366f7.apigw.ntruss.com/custom/v1/9901/a69a369fdbd87f3608f3220fd33807fff2462610ef09c67b7b22ff4a9d914f46/general";
+		String secretKey = "bWxaWmpLTmtDa0JnQ1Jsc2hBcU9ud2tSZ1d1eWFzb0Y=";
+		String imageFile = "c:C:\\Users\\LG\\Desktop\\eclipse_1\\test_ocr.png";
+
+		try {
+			URL url = new URL(apiURL);
+			HttpURLConnection con = (HttpURLConnection)url.openConnection();
+			con.setUseCaches(false);
+			con.setDoInput(true);
+			con.setDoOutput(true);
+			con.setReadTimeout(30000);
+			con.setRequestMethod("POST");
+			String boundary = "----" + UUID.randomUUID().toString().replaceAll("-", "");
+			con.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
+			con.setRequestProperty("X-OCR-SECRET", secretKey);
+
+			JSONObject json = new JSONObject();
+			json.put("version", "V2");
+			json.put("requestId", UUID.randomUUID().toString());
+			json.put("timestamp", System.currentTimeMillis());
+			JSONObject image = new JSONObject();
+			image.put("format", "png");
+			image.put("name", "test2");
+			JSONArray images = new JSONArray();
+			images.put(image);
+			json.put("images", images);
+			String postParams = json.toString();
+
+			con.connect();
+			DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+			long start = System.currentTimeMillis();
+			File file = new File(imageFile);
+			writeMultiPart(wr, postParams, file, boundary);
+			wr.close();
+
+			int responseCode = con.getResponseCode();
+			BufferedReader br;
+			if (responseCode == 200) {
+				br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			} else {
+				br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+			}
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+			while ((inputLine = br.readLine()) != null) {
+				response.append(inputLine);
+			}
+			br.close();
+
+			System.out.println(response);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	private static void writeMultiPart(OutputStream out, String jsonMessage, File file, String boundary) throws
+		IOException {
+		StringBuilder sb = new StringBuilder();
+		sb.append("--").append(boundary).append("\r\n");
+		sb.append("Content-Disposition:form-data; name=\"message\"\r\n\r\n");
+		sb.append(jsonMessage);
+		sb.append("\r\n");
+
+		out.write(sb.toString().getBytes("UTF-8"));
+		out.flush();
+
+		if (file != null && file.isFile()) {
+			out.write(("--" + boundary + "\r\n").getBytes("UTF-8"));
+			StringBuilder fileString = new StringBuilder();
+			fileString
+				.append("Content-Disposition:form-data; name=\"file\"; filename=");
+			fileString.append("\"" + file.getName() + "\"\r\n");
+			fileString.append("Content-Type: application/octet-stream\r\n\r\n");
+			out.write(fileString.toString().getBytes("UTF-8"));
+			out.flush();
+
+			try (FileInputStream fis = new FileInputStream(file)) {
+				byte[] buffer = new byte[8192];
+				int count;
+				while ((count = fis.read(buffer)) != -1) {
+					out.write(buffer, 0, count);
+				}
+				out.write("\r\n".getBytes());
+			}
+
+			out.write(("--" + boundary + "--\r\n").getBytes("UTF-8"));
+		}
+		out.flush();
+	}
+}
---
+++ springboot(2106)/Semi_Project/README.md
@@ -0,0 +1,33 @@
+# STOCK MAGAZINE
+
+
+
+## 목적
+
+### 관심있는 주식 선택 -> 산업에 대한 동향 파악 X
+
+### <u>관심있는 산업 선택 -> 관련 주식 소개 O</u>
+
+
+
+![1](C:/Users/LG/Desktop/eclipse_1/stock%20magazine/1.jpg)
+
+![2](C:/Users/LG/Desktop/eclipse_1/stock%20magazine/2.jpg)
+
+![3](C:/Users/LG/Desktop/eclipse_1/stock%20magazine/3.jpg)
+
+![4](C:/Users/LG/Desktop/eclipse_1/stock%20magazine/4.jpg)
+
+![5](C:/Users/LG/Desktop/eclipse_1/stock%20magazine/5.jpg)
+
+![6](C:/Users/LG/Desktop/eclipse_1/stock%20magazine/6.jpg)
+
+![7](C:/Users/LG/Desktop/eclipse_1/stock%20magazine/7.jpg)
+
+![8](C:/Users/LG/Desktop/eclipse_1/stock%20magazine/8.jpg)
+
+![9](C:/Users/LG/Desktop/eclipse_1/stock%20magazine/9.jpg)
+
+![10](C:/Users/LG/Desktop/eclipse_1/stock%20magazine/10.jpg)
+
+![11](C:/Users/LG/Desktop/eclipse_1/stock%20magazine/11.jpg)
